e0a0f6a14ccd2afc4fa57d600a878a8eff9b019a
lea84110 <55747854+lea84110@users.noreply.github.com>
2019-09-24 15:56:30 +0200

    Add files via upload

---
+++ atelier1.R
@@ -0,0 +1,32 @@
+2 + 3
+8 - 12
+14 * 25
+-3/10
+1-sqrt(2)
+sort(c(2,5,1,3))
+3*
+5  
+x <- 2
+x
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+length(tailles)
+mean(tailles) #moyenne
+var(tailles) #variance
+sd(tailles) #écart-type
+summary(tailles) #resume
+hist(tailles) #faireunhistogramme
+tailles.m <- tailles/100 #convertirenm
+tailles.m = tailles/100
+tailles/100 -> tailles.m
+tailles.m
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(poids)
+mean(poids)
+sd(poids)
+summary(poids)
+imc <- poids/tailles.m^2
+imc
+donnees <- data.frame(tailles,poids,imc)
